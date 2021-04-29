20c32a43df52e5c7c6329586a49d1c0ef4b3a01a
Sebastian Manrique <yhoan.manrique@correo.uis.edu.co>
2021-04-28 21:32:03 -0500

    ✨ (transactions hisotry page): transactions
    list transactions item and transactions headings

--- lib/theme.dart
+++ lib/theme.dart
@@ -2,5 +2,12 @@ import 'package:finance_app/extensions.dart';
 import 'package:flutter/material.dart';
 
 class FinanceTheme {
-  static Map<String, Color> colors = {'primary': HexColor.fromHex('#FF3131')};
+  static Map<String, Color> colors = {
+    'primary': HexColor.fromHex('#FF3131'),
+    'black': HexColor.fromHex('003049'),
+    'secondary': HexColor.fromHex('D90429'),
+    'terciary': HexColor.fromHex('48CAE4'),
+    'gray': HexColor.fromHex('D7D8DA'),
+    'success': HexColor.fromHex('06D6A0')
+  };
 }
