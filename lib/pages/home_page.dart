8607f4d61713c6799679d4884e5a38039577fa38
DanielSarmientoDev <danielsarmientodeveloper@gmail.com>
2021-04-30 14:50:36 -0500

    missing style

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,40 @@
+import 'package:finance_app/theme.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
       home: Scaffold(
+        backgroundColor: FinanceTheme.colors['primary'],
         body: Center(
-          child: Text('home page'),
-        ),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+              margin: EdgeInsets.only(bottom: 30.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        'Balance total',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 34.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Text(
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ullamcorper tellus morbi lobortis laoreet ut odio cursus posuere.'),
+                ],
+              ),
+            ),
+          ],
+        )),
       ),
     );
   }
