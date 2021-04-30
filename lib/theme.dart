0b5368de1126d06c83df5e72600b83748d6cfce3
DanielSarmientoDev <danielsarmientodeveloper@gmail.com>
2021-04-30 14:36:00 -0500

    update pallete colors

--- lib/theme.dart
+++ lib/theme.dart
@@ -2,5 +2,13 @@ import 'package:finance_app/extensions.dart';
 import 'package:flutter/material.dart';
 
 class FinanceTheme {
-  static Map<String, Color> colors = {'primary': HexColor.fromHex('#FF3131')};
+  static Map<String, Color> colors = {
+    'black': HexColor.fromHex('#003049'),
+    'primary': HexColor.fromHex('#FF3131'),
+    'success': HexColor.fromHex('#06D6A0'),
+    'gray': HexColor.fromHex('#D7D8DA'),
+    'secondary': HexColor.fromHex('#D90429'),
+    'terciary': HexColor.fromHex('#48CAE4'),
+    'white': HexColor.fromHex('#ffffff')
+  };
 }
