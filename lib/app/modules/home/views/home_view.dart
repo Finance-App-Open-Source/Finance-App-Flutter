1cad6134949e72810791947a4a0c9b990d175f68
Sebastian Manrique <yhoan.manrique@correo.uis.edu.co>
2021-08-07 22:58:28 -0500

    ✨ (home): account card list view

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,8 +1,6 @@
 import 'package:finance_app/theme.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -14,10 +12,48 @@ class HomeView extends GetView<HomeController> {
         title: Text('HomeView'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      body: Container(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Lista de cuentas',
+              style: TextStyle(
+                color: FinanceTheme.colors['black'],
+                fontSize: 21,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 5.0,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 5.0),
+              height: MediaQuery.of(context).size.height * 0.30,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 3,
+                itemBuilder: (context, i) => AccountCard(),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AccountCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      width: MediaQuery.of(context).size.width * 0.5,
+      decoration: BoxDecoration(
+        color: Colors.amber,
+        borderRadius: BorderRadius.all(
+          Radius.circular(10.0),
         ),
       ),
     );
