58a8f570659b1efea4a0e3539ed4692dfa8ef819
Sebastian Manrique <yhoan.manrique@correo.uis.edu.co>
2021-09-12 23:12:35 -0500

    feat: styled button

--- lib/app/global_widgets/app_button.dart
+++ lib/app/global_widgets/app_button.dart
@@ -1,35 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:styled_widget/styled_widget.dart';
 
-class AppButton extends StatelessWidget {
+class AppButton extends StatefulWidget {
   final Function()? onTab;
   final Widget child;
   AppButton({Key? key, this.onTab, required this.child}) : super(key: key);
 
   @override
+  State<AppButton> createState() => _AppButtonState();
+}
+
+class _AppButtonState extends State<AppButton> {
+  bool _isPressed = false;
+  @override
   Widget build(BuildContext context) {
     var screenSize = MediaQuery.of(context).size;
-    return GestureDetector(
-      onTap: this.onTab,
-      child: Container(
-        height: 55.0,
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-          color: Colors.red,
-          borderRadius: BorderRadius.all(
-            Radius.circular(10.0),
-          ),
-          boxShadow: <BoxShadow>[
-            BoxShadow(
-              color: Colors.red.withOpacity(0.5),
-              blurRadius: 21,
-              offset: Offset(0, 5),
-            ),
-          ],
-        ),
-        padding: EdgeInsets.all(10.0),
-        width: screenSize.width,
-        child: child,
-      ),
-    );
+
+    final button = ({required Widget child}) => Styled.widget(child: child)
+        .padding(all: 10.0)
+        .alignment(Alignment.center)
+        .ripple()
+        .clipRRect(all: 10.0)
+        .decorated(
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.red.withOpacity(_isPressed ? 0 : 0.5),
+                blurRadius: 21,
+                offset: Offset(0, 5),
+              )
+            ])
+        .constrained(height: 55.0, width: screenSize.width)
+        .gestures(
+          onTap: widget.onTab,
+          onTapChange: (tapStatus) => setState(() => _isPressed = tapStatus),
+        )
+        .scale(all: _isPressed ? 0.95 : 1.0, animate: true)
+        .animate(Duration(milliseconds: 150), Curves.easeOut);
+
+    return button(child: widget.child);
   }
 }
